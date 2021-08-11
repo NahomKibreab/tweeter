58d013e2f2eac5fd7c23736143b2749e06b706a2
Nahom Mehanzel <nknahom@gmail.com>
2021-08-11 23:09:30 +0000

    Fetching tweets with Ajaxx completed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,30 +7,6 @@
 $(() => {
   // Test / driver code (temporary). Eventually will get this from the server.
   // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
 
   const createTweetElement = (data) => {
     const $tweet = $(`
@@ -71,7 +47,10 @@ $(() => {
     }
   };
 
-  renderTweets(data);
+  // request json file as a return value
+  $.getJSON("/tweets", (data) => {
+    renderTweets(data);
+  });
 
   // Listener for Submit Event
   $("form").submit(function (event) {
